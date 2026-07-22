75c88534a09e8455434a2b280e624ac53e5438da
Mahibalan A <lakshmiprabha.a1984@gmail.com>
2026-07-23 01:49:56 +0530

    Use DATABASE_URL for Render

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,11 +9,7 @@ import * as schema from './schema.ts';
 // Function to create a new connection pool using the Object Method (required)
 export const createPool = () => {
   return new Pool({
-    host: process.env.SQL_HOST,
-    port: Number(process.env.SQL_PORT),
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DB_NAME,
+    connectionString: process.env.DATABASE_URL,
     connectionTimeoutMillis: 15000,
   });
 };
